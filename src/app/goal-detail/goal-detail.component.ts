c11ac9a833e2a1eb32ed52ea7605be1a6799c531
mwaurameshack64 <mwaurameshack64@gmail.com>
2020-03-25 12:51:12 +0300

    modify goal detail

--- src/app/goal-detail/goal-detail.component.ts
+++ src/app/goal-detail/goal-detail.component.ts
@@ -7,16 +7,21 @@ import { Goal } from '../goal';
   styleUrls: ['./goal-detail.component.css']
 })
 export class GoalDetailComponent implements OnInit {
-
+  
   @Input() goal: Goal;
   @Output() isComplete = new EventEmitter<boolean>();
+
+
+  goalComplete(complete:boolean){
+    this.isComplete.emit(complete);
+  }
 
   goalDelete(complete:boolean){
     this.isComplete.emit(complete);
   }
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
 }
