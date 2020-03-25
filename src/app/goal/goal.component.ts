af6c244f436cb749534a6089e4af955aaceaa968
mwaurameshack64 <mwaurameshack64@gmail.com>
2020-03-25 12:52:48 +0300

    modify the goals component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Goal} from '../goal'
+
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
@@ -7,26 +8,45 @@ import {Goal} from '../goal'
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-   new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son'),
-   new Goal(2,'Buy Cookies','I have to buy cookies for the parrot'),
-   new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon'),
-   new Goal(4,'Get Dog Food','Pupper likes expensive snacks'),
-   new Goal(5,'Solve math homework','Damn Math'),
-   new Goal(6,'Plot my world domination plan','Cause I am an evil overlord'),
- ]; 
- 
- toggleDetails(index){
+    new Goal(1, 'Watch finding Nemo', 'Find an online version and watch merlin find his son',new Date(2020,3,14)),
+    new Goal(2,'Buy Cookies','I have to buy cookies for the parrot',new Date(2019,6,9)),
+    new Goal(3,'Get new Phone Case','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Goal(4,'Get Dog Food','Pupper likes expensive snacks',new Date(2019,0,18)),
+    new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
+    new Goal(6,'Plot my world domination plan','Cause I am an evil overlord',new Date(2030,3,14)),
+  ];
+
+
+  addNewGoal(goal){
+    let goalLength = this.goals.length;
+    goal.id = goalLength+1;
+    goal.completeDate = new Date(goal.completeDate)
+    this.goals.push(goal)
+  }
+
+  toggleDetails(index){
     this.goals[index].showDescription = !this.goals[index].showDescription;
   }
+
+  completeGoal(isComplete, index){
+    if (isComplete) {
+      this.goals.splice(index,1);
+    }
+  }
+
   deleteGoal(isComplete, index){
     if (isComplete) {
       let toDelete = confirm(`Are you sure you want to delete ${this.goals[index].name}?`)
+
       if (toDelete){
         this.goals.splice(index,1)
       }
     }
   }
+
   constructor() { }
+
   ngOnInit(): void {
   }
+
 }
